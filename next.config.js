973a6adf06236e72cd770522b39d87742b15fc27
yayeet999 <serenityai904@gmail.com>
2024-12-16 13:20:28 -0600

    Fix: Configure Next.js output for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'public',
+  images: {
+    unoptimized: true
+  },
   swcMinify: true,
   async rewrites() {
     return [
